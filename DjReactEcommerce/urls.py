b384c606453049e28c1111413c06491026bb8848
andy1729 <ujjawalanand1729@gmail.com>
2020-12-10 13:12:43 +0530

    comleted rest authentication using react

--- DjReactEcommerce/urls.py
+++ DjReactEcommerce/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
