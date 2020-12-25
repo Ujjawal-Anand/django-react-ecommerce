7a03af898244af5b7a9069d7c7fa6f29a953fe6a
Ujjawal Anand <ujjawalanand1729@gmail.com>
2020-12-25 21:07:14 +0530

    added cart redux store

---
+++ src/store/actions/cart.js
@@ -0,0 +1,36 @@
+import { CART_FAIL, CART_START, CART_SUCCESS } from "./actionTypes";
+import { authAxios } from "../../utils";
+import { orderSummaryURL } from "../../constants";
+
+export const cartStart = () => {
+    return {
+        type: CART_START
+    };
+};
+
+export const cartSuccess = data => {
+    return {
+        type: CART_SUCCESS,
+        data
+    };
+};
+
+export const cartFail = error => {
+    return {
+        type: CART_FAIL,
+        error
+    };
+};
+
+export const fetchCart = () => {
+    return dispatch => {
+        dispatch(cartStart());
+        authAxios.get(orderSummaryURL)
+                 .then(res => {
+                     dispatch(cartSuccess(res.data));
+                 })
+                 .catch(err => {
+                     dispatch(cartFail(err))
+                 });
+    };
+};
